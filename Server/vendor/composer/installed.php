fb59c571c5852757fefa639f7bdcf4c6c554f025
Niel <baloloycrisniel@gmail.com>
2025-02-26 17:30:00 +0800

    LBRMSS-7-phpspreadsheet integration

---
+++ Server/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'thingengineer/mysqli-database-class',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1745d1c1b32b9e67d6baa7b410cdb6ce24d5ec49',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'thingengineer/mysqli-database-class' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1745d1c1b32b9e67d6baa7b410cdb6ce24d5ec49',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
